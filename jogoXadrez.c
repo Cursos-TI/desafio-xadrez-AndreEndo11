e50f8e08970d46cd20a66518f50416179ae9daf1
AndreEndo11 <avieiraendo@gmail.com>
2025-05-14 10:02:13 -0300

    Update jogoXadrez.c

--- jogoXadrez.c
+++ jogoXadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int option, c = 1;
+    int option, c = 1, b = 1;
     // Enquanto a option for 1 ou 2 o jogo continua, quando option for 3 sai do jogo.
     do {
         printf("### JOGO XADREZ ###\n");
@@ -11,31 +11,41 @@ int main() {
         printf("3. Sair\n");
         printf("Escolha uma opção:\n");
         scanf("%d", &option);
-    
+        
+        printf("\n");
         switch(option) {
             case 1:
                 printf("### START ###\n");
                 // Mover a torre 5 casas para a direita
                 for (int a = 0; a < 5; a++) {
-                    printf("Torre Direita(->)\n"); // Imprime a direção do movimento
+                    printf("Torre Direita\n"); // Imprime a direção do movimento
                 }
-                printf("--------------------------------------------------\n");
+                printf("\n");
                 // Move o bispo 5 casas na diagonal
                 for (int b = 0; b < 5; b++) {
-                    printf("Bispo Diagonal(/)\n"); // Imprime a direção do movimento
+                    printf("Bispo Diagonal\n"); // Imprime a direção do movimento
                 }
-                printf("--------------------------------------------------\n");
+                printf("\n");
                 // Move a Rainha 8 casas para a esquerda
                 while (c <= 8) {
-                    printf("Rainha Esquerda(<-)\n"); // Imprime a direção do movimento
+                    printf("Rainha Esquerda\n"); // Imprime a direção do movimento
                     c++;        
                 }
-                printf("--------------------------------------------------\n");
+                printf("\n");
+                // Move o cavalo 2  para baixo e 1 para a esquerda
+                while (b--) {
+                    for (int i = 0; i < 2; i++) {
+                        printf("Cavalo Baixo\n"); // imprime "Baixo" duas vezes
+                    }
+                    printf("Cavalo Esquerda\n"); // Imprime "Esquerda" uma vez
+                }
+                printf("\n");
                 break;
             case 2:
                 printf("Torre: Move-se em linha reta horizontalmente ou verticalmente.\n");
                 printf("Bispo: Move-se na diagonal.\n");
                 printf("Rainha: Move-se em todas as direções.\n");
+                printf("Cavalo: se move duas casas em uma direção (horizontal ou vertical) e depois uma casa perpendicularmente\n");
                 break;
             case 3:
                 printf("Saindo...\n");
